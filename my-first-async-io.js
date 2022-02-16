98f2d59563e501eb4bcf95a82ccbd2b22933d20e
TI SATC <deptotisatc@gmail.com>
2022-02-16 20:59:26 -0300

    feat: learnyounode my-first-async-io.js

---
+++ my-first-async-io.js
@@ -0,0 +1,38 @@
+'use strict'
+const fs = require('fs')
+const file = process.argv[2]
+
+fs.readFile(file, function (err, contents) {
+    if (err) {
+    return console.log(err)
+    }
+    // fs.readFile(file, 'utf8', callback) can also be used
+    const lines = contents.toString().split('\n').length - 1
+    console.log(lines)
+})
+
+//---------------------
+
+// 'use strict'
+// const fs = require('fs')
+
+// var lines = undefined
+
+// function getLines(callback){
+//     fs.readFile(process.argv[2], (err, buffer) => {
+//         const linesCount = buffer.toString().split('\n').length - 1;
+//         lines = linesCount;
+//         callback();
+//     })
+// }
+
+// function logLines(){
+//     console.log(lines)
+// }
+
+// getLines(logLines);
+
+// note you can avoid the .toString() by passing 'utf8' as the
+// second argument to readFileSync, then you'll get a String!
+//
+// fs.readFileSync(process.argv[2], 'utf8').split('\n').length - 1
