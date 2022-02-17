1866e6fd60ad623b97bbb77b0f77a0d4bcbc5584
Gabriel Martins Padoin <gabrielmpwork@gmail.com>
2022-02-17 05:43:55 -0300

    feat: commentary insertion in my-first-async-io.js

--- my-first-async-io.js
+++ my-first-async-io.js
@@ -1,17 +1,35 @@
+// Flag that warns and prohibits bad quality JS Code from being compiled/executed
 'use strict'
+
+// Importing File System library for file manipulation
 const fs = require('fs')
+
+// We assign the file name value onto the "file" const.
 const file = process.argv[2]
 
-fs.readFile(file, function (err, contents) {
-    if (err) {
-    return console.log(err)
-    }
+// We read the file
+fs.readFile(file, 
+    // Execute the function and if there is an error during the file reading...
+    function (err, contents) {
+        // We log the error onto the console/terminal
+        if (err) {
+            return console.log(err)
+        }
+
+    /* Else, we turn the content of the file into a string, 
+       and get the sum count of all new lines present in that file
+       and we assign that sum value onto the "lines" const */
+
     // fs.readFile(file, 'utf8', callback) can also be used
     const lines = contents.toString().split('\n').length - 1
+
+    // console.log that will show us the count of all new lines
     console.log(lines)
 })
 
 //---------------------
+
+// Alternative solution
 
 // 'use strict'
 // const fs = require('fs')
