759712d4ca64035c9c9d45152553c576f8d85d36
Gabriel Martins Padoin <gabrielmpwork@gmail.com>
2022-02-17 05:30:35 -0300

    feat: commentary insertion in baby-steps.js

--- baby-steps.js
+++ baby-steps.js
@@ -1,17 +1,22 @@
+// Flag that warns and prohibits bad quality JS Code from being compiled/executed
 'use strict'
 
+// The let variable that will hold the sum of all numeric argument values
 let result = 0
 
+// for loop that will iterate and sum through all numeric argument values
 for (let i = 2; i < process.argv.length; i++) {
     result += Number(process.argv[i])
 }
 
+// console.log that will show us the sum of all numeric argument values
 console.log(result)
 
+// ---
+
+// Alternative solution
+
 // let allItems = process.argv;
-
 // allItems.splice(0, 2);
-
 // const total = allItems.reduce((accumulator, value) => parseInt(accumulator) + parseInt(value)) 
-
 // console.log(total);
